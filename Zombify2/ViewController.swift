0bf7627db1d5aa98b95a92ba89e308b327383b7d
Ramon Lopez <Rayy@ramons-mbp.attlocal.net>
2015-11-13 19:30:37 -0800

    Added files for zombify2

--- Zombify2/ViewController.swift
+++ Zombify2/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var zomPik: UIImageView!
+    @IBOutlet weak var zomBtn: UIButton!
+    @IBOutlet weak var zompact: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func zomify(sender: AnyObject) {
+        zomPik.hidden = false
+        zompact.hidden = false
+        zomBtn.hidden = true
+    }
 }
 
